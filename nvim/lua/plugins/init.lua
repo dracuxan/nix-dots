dd0cba8703d170f6de524ad5645a53a16479c0cc
dracuxan <nisargkhodke@gmail.com>
2025-05-07 01:03:08 +0530

    rm: unused lsp servers and lazy plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -262,25 +262,6 @@ local custom_plugins = {
 		"mfussenegger/nvim-dap",
 	},
 	{
-		"saecki/crates.nvim",
-		ft = { "toml" },
-		config = function(_, opts)
-			local crates = require("crates")
-			crates.setup(opts)
-			require("cmp").setup.buffer({
-				sources = { { name = "crates" } },
-			})
-			crates.show()
-		end,
-	},
-	{
-		"rust-lang/rust.vim",
-		ft = "rust",
-		init = function()
-			vim.g.rustfmt_autosave = 1
-		end,
-	},
-	{
 		"theHamsta/nvim-dap-virtual-text",
 		lazy = false,
 		config = function(_, opts)

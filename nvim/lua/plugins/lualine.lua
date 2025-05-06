98249a5f2eae72712b170e3b22d70475aa10dc75
dracuxan <nisargkhodke@gmail.com>
2025-05-07 00:11:26 +0530

    fix: hide lualine for toogleterm prompts

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -37,7 +37,7 @@ local conditions = {
 local config = {
 	options = {
 		globalstatus = true,
-		disabled_filetypes = { "alpha", "TelescopePrompt" },
+		disabled_filetypes = { "alpha", "TelescopePrompt", "toggleterm" },
 		-- Disable sections and component separators
 		component_separators = "",
 		section_separators = "",

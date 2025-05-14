faaa2a1b856ac7650e3e7b5f474cff1e253ddddd
dracuxan <nisargkhodke@gmail.com>
2025-05-14 22:45:59 +0530

    fix: formatting

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -44,8 +44,8 @@ vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
 vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
 vim.keymap.set("n", "<leader>x", function()
-	local bufnr = vim.api.nvim_get_current_buf() -- Get the current buffer number
-	local buffers = vim.fn.getbufinfo({ buflisted = 1 }) -- Get list of open buffers
+	local bufnr = vim.api.nvim_get_current_buf()          -- Get the current buffer number
+	local buffers = vim.fn.getbufinfo({ buflisted = 1 })  -- Get list of open buffers
 
 	if #buffers > 1 then
 		vim.cmd("bnext") -- Switch to the next buffer

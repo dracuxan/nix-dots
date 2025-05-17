c5051e7c0fae6e27495362e9ea2f732367a73455
dracuxan <nisargkhodke@gmail.com>
2025-05-18 00:05:10 +0530

    feat: use better imports organizer function for gopls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -165,10 +165,18 @@ return {
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					pattern = "*.go",
 					callback = function()
-						vim.lsp.buf.code_action({
-							context = { only = { "source.organizeImports" } },
-							apply = true,
-						})
+						local params = vim.lsp.util.make_range_params()
+						params.context = { only = { "source.organizeImports" } }
+						local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+						for cid, res in pairs(result or {}) do
+							for _, r in pairs(res.result or {}) do
+								if r.edit then
+									local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+									vim.lsp.util.apply_workspace_edit(r.edit, enc)
+								end
+							end
+						end
+						vim.lsp.buf.format({ async = false })
 					end,
 				})
 			end,
@@ -195,11 +203,11 @@ return {
 				filetypes = { "go", "gomod", "gowork", "gotmpl" },
 				settings = {
 					gopls = {
-						completeUnimported = true,
-						usePlaceholders = true,
 						analyses = {
 							unusedparams = true,
 						},
+						staticcheck = true,
+						gofumpt = true,
 					},
 				},
 			},

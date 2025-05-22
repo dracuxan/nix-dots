bca4e90a307ce8091f53f0f8764d897a264429e0
dracuxan <nisargkhodke@gmail.com>
2025-05-22 20:48:40 +0530

    fix: toggle term functions and keymaps

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -36,10 +36,6 @@ function _G.set_terminal_keymaps()
 	local opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-	-- vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-	-- vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-	-- vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-	-- vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
@@ -62,6 +58,7 @@ function _RUN_SCRIPT()
 	-- Create a terminal instance with the dynamic command
 	local script_term = Terminal:new({
 		cmd = "run.sh " .. filename,
+		direction = "horizontal",
 		hidden = true,
 		close_on_exit = false,
 	})

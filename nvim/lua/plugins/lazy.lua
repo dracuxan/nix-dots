e5d3ba676993209f03c695cb9b7a4b2251b7373d
dracuxan <dracuxan@proton.me>
2025-10-03 23:49:19 +0530

    fix: comments in visual mode

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -207,6 +207,12 @@ local custom_plugins = {
 		config = function()
 			local opts = { noremap = true, silent = true }
 			vim.keymap.set("n", "<C-c>", require("Comment.api").toggle.linewise.current, opts)
+			vim.keymap.set(
+				"v",
+				"<C-c>",
+				"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+				opts
+			)
 		end,
 	},
 
